0758cc7875ef3ee564f831a35549692ce32977d8
BlitzFire <ninadpatilbvp@gmail.com>
2017-06-01 20:18:40 +0530

    Update full_aio_otfp.mk
    Fixed Compiling Error

--- full_aio_otfp.mk
+++ full_aio_otfp.mk
@@ -14,33 +14,13 @@
 # limitations under the License.
 #
 
-# Product common configurations
-# 64-bit support
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
-
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/lenovo/aio_otfp/device.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := aio_otfp
-
-EXTENDED_FONT_FOOTPRINT := true
-
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aio_otfp
 PRODUCT_NAME := full_aio_otfp
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := K3 Note
+PRODUCT_MODEL := Lenovo K3 Note
 PRODUCT_MANUFACTURER := Lenovo
-PRODUCT_RESTRICT_VENDOR_FILES := false
-
-# Boot animation
-TARGET_SCREEN_HEIGHT      := 1920
-TARGET_SCREEN_WIDTH       := 1080
-TARGET_BOOTANIMATION_NAME := 1080
-
-PRODUCT_DEFAULT_LANGUAGE := en
-PRODUCT_DEFAULT_REGION   := US
+PRODUCT_BOARD := aio_otfp
